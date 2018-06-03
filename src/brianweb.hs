379c56b93e62429442a9a3c5d3a608862a7abed2
Brian Clark <bclarkx2@gmail.com>
2018-06-03 15:20:45 -0400

    Remove unnecessary import

--- src/brianweb.hs
+++ src/brianweb.hs
@@ -5,7 +5,6 @@ module Main where
 {- Imports -}
 import Happstack.Server
 import Control.Monad
-import qualified Control.Concurrent.Thread.Group as TG
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html4.Strict as H
 import qualified Text.Blaze.Html4.Strict.Attributes as A
