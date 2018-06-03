6b7ea540796fbad62d285d34dfdf1a41a0a0e2c5
Brian Clark <bclarkx2@gmail.com>
2018-06-03 11:31:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# brianweb
+
+My website!
---
+++ src/brianweb.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+{- Imports -}
+import Happstack.Server
+import Control.Monad
+import qualified Control.Concurrent.Thread.Group as TG
+import Text.Blaze ((!))
+import qualified Text.Blaze.Html4.Strict as H
+import qualified Text.Blaze.Html4.Strict.Attributes as A
+
+{- Conf -}
+conf :: Conf
+conf = Conf { port = 8888
+            , validator = Nothing
+            , logAccess = Just logMAccess
+            , timeout = 30
+            , threadGroup = Nothing
+          }
+
+{- Main -}
+main :: IO ()
+main = simpleHTTP conf $ handlers
+
+handlers = msum
+  [ dir "hello" $ do method [GET, HEAD]
+                     greet
+  , ok $ toResponse $ homePage
+  ]
+
+{- Responses -}
+greet = path (\s -> ok $ toResponse $ "Hello, " ++ s ++ "\n")
+
+
+{- Templates -}
+appTemplate :: String -> [H.Html] -> H.Html -> H.Html
+appTemplate title headers body =
+    H.html $ do
+      H.head $ do
+        H.title (H.toHtml title)
+        H.meta ! A.httpEquiv "Content-Type" ! A.content "text/html;charset=utf-8"
+        sequence_ headers
+      H.body $ do
+        body
+
+{- Pages -}
+homePage :: H.Html
+homePage = appTemplate "h o me pag e" 
+                       [H.meta ! A.name "keywords" ! A.content "happstack, blaze, html"] 
+                       (H.p "HOME!")
